d1b0209b75fe8ce1a50b6dc2d7aff50d31ecda97
zipper <oleg@pavloff.de>
2018-03-10 13:02:42 +0100

    plugin as tool window

---
+++ src/de/pavloff/recomcode/core/plugin/RecomCodeProjectComponent.java
@@ -0,0 +1,13 @@
+package de.pavloff.recomcode.core.plugin;
+
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.wm.ToolWindow;
+import com.intellij.openapi.wm.ToolWindowFactory;
+import org.jetbrains.annotations.NotNull;
+
+public class RecomCodeProjectComponent implements ToolWindowFactory {
+
+    @Override
+    public void createToolWindowContent(@NotNull Project project, @NotNull ToolWindow toolWindow) {
+    }
+}
