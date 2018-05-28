4551d820a745e01cc6e774bd994bec55a117b241
o.pavloff <oleg@pavloff.de>
2018-05-28 17:30:18 +0200

    package refactoring

--- src/de/pavloff/recomcode/plugin/varviewer/actions/RunCodeAction.java
+++ src/de/pavloff/recomcode/plugin/varviewer/actions/RunCodeAction.java
@@ -1,4 +1,4 @@
-package de.pavloff.recomcode.plugin.actions;
+package de.pavloff.recomcode.plugin.varviewer.actions;
 
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
