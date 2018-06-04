f1cb9ae290eaa2ccdd57195587c52175d4db7cdc
o.pavloff <oleg@pavloff.de>
2018-06-04 16:40:05 +0200

    core interfaces and classes

---
+++ src/de/pavloff/recomcode/core/CodeFragment.java
@@ -0,0 +1,20 @@
+package de.pavloff.recomcode.core;
+
+public class CodeFragment {
+
+    private String recID;
+    private String group;
+    private String parent;
+    private String[] related;
+    private String[] textkey;
+    private String[] keywords;
+    private String commentTemplate;
+    private String sources;
+    private String documentation;
+    private String code;
+    private CodeParam[] parameters;
+
+    public CodeFragment() {
+
+    }
+}
