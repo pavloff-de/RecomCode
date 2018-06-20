324d1220ff6697892e831baeadcde524e982f86b
o.pavloff <oleg@pavloff.de>
2018-06-20 17:57:06 +0200

    mroe dataframe code fragments

--- test/TestYamlLoader.java
+++ test/TestYamlLoader.java
@@ -8,16 +8,23 @@ import java.util.ArrayList;
 
 public class TestYamlLoader {
 
-    private String ressourcePath = "test/ressources/test-fragments.yml";
     private YamlLoader yamlReader = new YamlLoader();
 
     @Test
     public void testLoad() {
-        File[] files = {new File(ressourcePath)};
+        File[] files = {new File("test/ressources/test-fragments.yml")};
         ArrayList<CodeParam> params = yamlReader.getCodeParams(files);
         ArrayList<CodeFragment> fragments = yamlReader.getCodeFragments(files);
 
         assert params.size() == 2;
         assert fragments.size() == 3;
+    }
+
+    @Test
+    public void testLoadDF() {
+        File[] files = {new File("src/de/pavloff/pycharm/yaml/resources/fragments-dataframe.yml")};
+        ArrayList<CodeFragment> fragments = yamlReader.getCodeFragments(files);
+
+        assert fragments.size() == 5;
     }
 }
