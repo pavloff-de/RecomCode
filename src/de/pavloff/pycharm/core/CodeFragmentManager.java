1cce2be93bed48b553c8336afc75b062fba3a44f
o.pavloff <oleg@pavloff.de>
2018-09-03 18:03:20 +0200

    use a pool of worker

--- src/de/pavloff/pycharm/core/CodeFragmentManager.java
+++ src/de/pavloff/pycharm/core/CodeFragmentManager.java
@@ -2,9 +2,6 @@ package de.pavloff.pycharm.core;
 
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.Pair;
-import de.pavloff.pycharm.core.worker.AprioriWorker;
-import de.pavloff.pycharm.core.worker.HistoryWorker;
-import de.pavloff.pycharm.core.worker.KeywordWorker;
 import de.pavloff.pycharm.core.worker.Worker;
 import de.pavloff.pycharm.yaml.YamlLoader;
 
@@ -16,9 +13,7 @@ public class CodeFragmentManager implements Worker {
     // TODO: make configurable
     private CodeFragmentLoader loader = new YamlLoader();
 
-    private Worker worker1 = new KeywordWorker(loader);
-    private Worker worker2 = new AprioriWorker(loader);
-    private Worker worker3 = new HistoryWorker(loader);
+    private Map<String, Worker> workers = new HashMap<>();
 
     private List<CodeFragmentListener> codeFragmentListeners = new LinkedList<>();
 
@@ -30,38 +25,32 @@ public class CodeFragmentManager implements Worker {
         codeFragmentListeners.add(listener);
     }
 
-    public CodeFragmentLoader getLoader() {
+    public void addWorker(Worker worker) {
+        workers.put(worker.workerName(), worker);
+    }
+
+    CodeFragmentLoader getLoader() {
         return loader;
     }
 
     private void returnRecommendations() {
         CodeFragment.FragmentSorter sorter = new CodeFragment.FragmentSorter();
 
-        LinkedHashSet<CodeFragment> recommendation1 = worker1.getRecommendation();
-        Iterator<CodeFragment> it = recommendation1.iterator();
-        int r = recommendation1.size();
-        while (it.hasNext()) {
-            sorter.add(it.next(), r);
-            r--;
+        LinkedHashSet<CodeFragment> recommendation;
+        Iterator<CodeFragment> it;
+        int rank;
+
+        for (Worker worker : workers.values()) {
+            recommendation = worker.getRecommendation();
+            it = recommendation.iterator();
+            rank = recommendation.size();
+            while (it.hasNext()) {
+                sorter.add(it.next(), rank);
+                rank--;
+            }
         }
 
-        LinkedHashSet<CodeFragment> recommendation2 = worker2.getRecommendation();
-        it = recommendation2.iterator();
-        r = recommendation2.size();
-        while (it.hasNext()) {
-            sorter.add(it.next(), r);
-            r--;
-        }
-
-        LinkedHashSet<CodeFragment> recommendation3 = worker3.getRecommendation();
-        it = recommendation3.iterator();
-        r = recommendation3.size();
-        while (it.hasNext()) {
-            sorter.add(it.next(), r);
-            r--;
-        }
-
-        LinkedHashSet<CodeFragment> recommendation = sorter.sortFragments();
+        recommendation = sorter.sortFragments();
         for (CodeFragmentListener listener : codeFragmentListeners) {
             listener.onOutput(recommendation);
         }
@@ -79,64 +68,64 @@ public class CodeFragmentManager implements Worker {
 
     @Override
     public void onInput(String input) {
-        worker1.onInput(input);
-        worker2.onInput(input);
-        worker3.onInput(input);
+        for (Worker worker : workers.values()) {
+            worker.onInput(input);
+        }
         returnRecommendations();
     }
 
     @Override
     public void dataframeSelected(TableModel table) {
-        worker1.dataframeSelected(table);
-        worker2.dataframeSelected(table);
-        worker3.dataframeSelected(table);
+        for (Worker worker : workers.values()) {
+            worker.dataframeSelected(table);
+        }
         returnRecommendations();
     }
 
     @Override
     public void cellSelected(int row, int column) {
-        worker1.cellSelected(row, column);
-        worker2.cellSelected(row, column);
-        worker3.cellSelected(row, column);
+        for (Worker worker : workers.values()) {
+            worker.cellSelected(row, column);
+        }
         returnRecommendations();
     }
 
     @Override
     public void cellsSelected(List<Pair<Integer, Integer>> cells) {
-        worker1.cellsSelected(cells);
-        worker2.cellsSelected(cells);
-        worker3.cellsSelected(cells);
+        for (Worker worker : workers.values()) {
+            worker.cellsSelected(cells);
+        }
         returnRecommendations();
     }
 
     @Override
     public void rowSelected(int row) {
-        worker1.rowSelected(row);
-        worker2.rowSelected(row);
-        worker3.rowSelected(row);
+        for (Worker worker : workers.values()) {
+            worker.rowSelected(row);
+        }
         returnRecommendations();
     }
 
     @Override
     public void columnSelected(int column) {
-        worker1.columnSelected(column);
-        worker2.columnSelected(column);
-        worker3.columnSelected(column);
+        for (Worker worker : workers.values()) {
+            worker.columnSelected(column);
+        }
         returnRecommendations();
     }
 
     @Override
     public void codeFragmentSelected(CodeFragment fragment) {
-        worker1.codeFragmentSelected(fragment);
-        worker2.codeFragmentSelected(fragment);
-        worker3.codeFragmentSelected(fragment);
+        for (Worker worker : workers.values()) {
+            worker.codeFragmentSelected(fragment);
+        }
     }
 
     @Override
     public void sourceCode(String code) {
-        worker1.sourceCode(code);
-        worker2.sourceCode(code);
-        worker3.sourceCode(code);
+        for (Worker worker : workers.values()) {
+            worker.sourceCode(code);
+        }
     }
 
     @Override
--- src/de/pavloff/pycharm/plugin/recomcode/RecomCodeProjectComponent.java
+++ src/de/pavloff/pycharm/plugin/recomcode/RecomCodeProjectComponent.java
@@ -3,6 +3,12 @@ package de.pavloff.pycharm.plugin.recomcode;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.wm.ToolWindow;
 import com.intellij.openapi.wm.ToolWindowFactory;
+import de.pavloff.pycharm.core.CodeFragmentLoader;
+import de.pavloff.pycharm.core.CodeFragmentManager;
+import de.pavloff.pycharm.core.worker.AprioriWorker;
+import de.pavloff.pycharm.core.worker.HistoryWorker;
+import de.pavloff.pycharm.core.worker.KeywordWorker;
+import de.pavloff.pycharm.yaml.YamlLoader;
 import org.jetbrains.annotations.NotNull;
 
 public class RecomCodeProjectComponent implements ToolWindowFactory {
@@ -11,5 +17,11 @@ public class RecomCodeProjectComponent implements ToolWindowFactory {
     public void createToolWindowContent(@NotNull Project project, @NotNull ToolWindow toolWindow) {
         RecomCodeManager manager = RecomCodeManager.getInstance(project);
         toolWindow.getComponent().add(manager.initView(project));
+
+        CodeFragmentLoader loader = new YamlLoader();
+        CodeFragmentManager recommender = CodeFragmentManager.getInstance(project);
+        recommender.addWorker(new KeywordWorker(loader));
+        recommender.addWorker(new AprioriWorker(loader));
+        recommender.addWorker(new HistoryWorker(loader));
     }
 }
