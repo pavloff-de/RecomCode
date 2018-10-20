73dbfd364426f2522e1f3a4fc6e2f9c36ec83419
zipper <oleg@pavloff.de>
2018-10-20 21:39:54 +0200

    type checking after search for psi elements deactivated *it takes a lot of time

--- src/de/pavloff/pycharm/plugin/macros/PyUniqueIterableVariableMacro.java
+++ src/de/pavloff/pycharm/plugin/macros/PyUniqueIterableVariableMacro.java
@@ -2,10 +2,18 @@ package de.pavloff.pycharm.plugin.macros;
 
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiNamedElement;
+import com.jetbrains.python.codeInsight.controlflow.ControlFlowCache;
+import com.jetbrains.python.codeInsight.controlflow.ScopeOwner;
+import com.jetbrains.python.codeInsight.dataflow.scope.Scope;
+import com.jetbrains.python.codeInsight.dataflow.scope.ScopeUtil;
 import com.jetbrains.python.codeInsight.liveTemplates.PyIterableVariableMacro;
+import com.jetbrains.python.psi.PyImplicitImportNameDefiner;
+import com.jetbrains.python.psi.PyTypedElement;
+import one.util.streamex.StreamEx;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.*;
+import java.util.stream.StreamSupport;
 
 public class PyUniqueIterableVariableMacro extends PyIterableVariableMacro {
 
@@ -24,15 +32,44 @@ public class PyUniqueIterableVariableMacro extends PyIterableVariableMacro {
         List<String> elementNames = new LinkedList<>();
         List<PsiNamedElement> uniqueComponents = new ArrayList<>();
 
-        for (PsiNamedElement component : super.getIterableElements(element)) {
-            String elementName = component.getName();
-            if (elementNames.contains(elementName)) {
-                continue;
+//        final TypeEvalContext typeEvalContext =
+//                TypeEvalContext.userInitiated(element.getProject(), element.getContainingFile());
+
+        for (PsiNamedElement namedElement : getVisibleNamedElements(element)) {
+            if (namedElement instanceof PyTypedElement) {
+                String elementName = namedElement.getName();
+
+                if (elementNames.contains(elementName)) {
+                    continue;
+                }
+
+                // FIXME: type checking takes a lot of time
+//                if (type != null && PyABCUtil.isSubtype(type, PyNames.ITERABLE,
+//                        typeEvalContext)) {
+                    elementNames.add(elementName);
+                    uniqueComponents.add(namedElement);
+//                }
             }
-            elementNames.add(elementName);
-            uniqueComponents.add(component);
         }
 
         return uniqueComponents;
+    }
+
+    @NotNull
+    private static List<PsiNamedElement> getVisibleNamedElements(@NotNull PsiElement anchor) {
+        final List<PsiNamedElement> results = new ArrayList<>();
+
+        for (ScopeOwner owner = ScopeUtil.getScopeOwner(anchor); owner != null; owner = ScopeUtil.getScopeOwner(owner)) {
+            final Scope scope = ControlFlowCache.getScope(owner);
+            results.addAll(scope.getNamedElements());
+
+            StreamEx
+                    .of(scope.getImportedNameDefiners())
+                    .filter(definer -> !(definer instanceof PyImplicitImportNameDefiner))
+                    .flatMap(definer -> StreamSupport.stream(definer.iterateNames().spliterator(), false))
+                    .select(PsiNamedElement.class)
+                    .forEach(results::add);
+        }
+        return results;
     }
 }
