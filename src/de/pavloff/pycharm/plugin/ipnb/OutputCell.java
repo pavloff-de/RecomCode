eb6ba98945fb4f0629783f0f1c1bde45708804d1
zipper <oleg@pavloff.de>
2018-11-14 10:23:04 +0100

    output cell refactoring, comments

--- src/de/pavloff/pycharm/plugin/ipnb/OutputCell.java
+++ src/de/pavloff/pycharm/plugin/ipnb/OutputCell.java
@@ -2,6 +2,10 @@ package de.pavloff.pycharm.plugin.ipnb;
 
 import java.util.List;
 
+/** Class simulates the {@link org.jetbrains.plugins.ipnb.format.cells.IpnbCell}
+ * It can be used to separate all possible outputs of the code execution
+ * @see org.jetbrains.plugins.ipnb.protocol.IpnbConnectionListenerBase for more info
+ */
 public interface OutputCell {
 
     void onOutput(List<String> output);
